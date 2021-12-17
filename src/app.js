8e942a07b77ed86ed13dd0536676dae5215ec5fb
dev-Rasla <thinesh@vinteksolution.com>
2021-12-17 18:02:01 +0530

    added pin and reply hi

--- src/app.js
+++ src/app.js
@@ -1,24 +1,38 @@
 const { Telegraf } = require("telegraf");
-const { gifs, onUserJoin, onUserLeft } = require("./helpers/basicUserFns");
+const {
+  onUserJoin,
+  onUserLeft,
+  pinMessage,
+  startBot,
+} = require("./helpers/userManagementActions");
 const { commands } = require("./helpers/allcommands");
-
 const { HTTP_API_KEY } = process.env;
 
 const bot = new Telegraf(HTTP_API_KEY);
 
-bot.start((ctx) => {
-  ctx.replyWithMarkdownV2(
-    "**Hai i am lazybot, made by rasla💖type /help for more comments🥰"
-  );
-});
-
-bot.help((ctx) => {
-  ctx.reply(commands.join(""));
-});
-
-bot.command("kiss", gifs);
+bot.start(startBot);
+bot.help((ctx) => ctx.reply(commands.join("")));
 
 bot.on("new_chat_members", onUserJoin);
 bot.on("left_chat_member", onUserLeft);
+
+bot.command("rules", (ctx) => ctx.reply(commands.join("")));
+bot.command("chatid", (ctx) =>
+  ctx.reply(
+    ctx.update.message.reply_to_message
+      ? ctx.update.message.chat.id
+      : "Hey Bro! 😂Specify a chat to get chatid"
+  )
+);
+bot.command("pin", (e) => pinMessage(e, bot));
+
+bot.command("getall", (ctx) => {
+  console.log(ctx);
+});
+
+bot.on("text", (ctx) => {
+  if (["Hello","Hi","Hey","Good morning","Good afternoon","Good evening","good night"].includes(ctx.))
+    ctx.reply("Hai");
+});
 
 module.exports = bot;
--- src/helpers/allcommands.js
+++ src/helpers/allcommands.js
@@ -1,8 +1,9 @@
 let commands = [
   "Welcome to Lazybot!💘\n Try the following comments🤗\n\n",
-  "/kiss : send's kissing gifs\n",
+  "/help : To list all available comments\n",
   "/hug : send's you back hug gifs\n",
-  "",
+  "/pin : Pin the message you reply to\n",
+  "/chatid : Get the chat id of specified chat\n",
 ];
 
 module.exports = {
