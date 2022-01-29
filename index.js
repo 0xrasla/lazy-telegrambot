d8868686031d4566929e97a870ebb024577b99e3
Rasla <thinesh.rasla@trustalchemy.com>
2022-01-29 20:11:13 +0530

    adding fix final commit

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ const app = express();
 const bot = require("./src/app");
 
 app.get("/", (req, res) => {
-  bot.launch();
-  console.log("dd");
   res.json({ ok: true, message: "Bot Running" });
 });
 
-app.listen(3000, () => {
-  console.log("App running in port 3000");
-});
-
+bot.launch();
 module.exports = app;
