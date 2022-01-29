4da021a57093a2221cec5d741e52d20887780df0
Rasla <thinesh.rasla@trustalchemy.com>
2022-01-29 19:54:38 +0530

    adding fix in express 2

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
-
 require("dotenv").config();
 const app = express();
 
-const express = require("express");
 const bot = require("./src/app");
 
 app.get("/", (req, res) => {
   bot.launch();
   console.log("dd");
   res.json({ ok: true, message: "Bot Running" });
+});
+
+app.listen(3000, () => {
+  console.log("App running in port 3000");
 });
 
 module.exports = app;
