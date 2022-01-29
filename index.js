364530206ca19d97d15d6d706cb14cdac9a92727
Rasla <programmer.rasla@gmail.com>
2022-01-29 20:23:43 +0530

    adding fix final commit v2

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const express = require("express");
 require("dotenv").config();
-const app = express();
 
 const bot = require("./src/app");
 
-app.get("/", (req, res) => {
-  res.json({ ok: true, message: "Bot Running" });
-});
-
 bot.launch();
-module.exports = app;
