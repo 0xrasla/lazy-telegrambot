e7cb9c2212e4da3236a50931eabc83081ecf011c
Rasla <programmer.rasla@gmail.com>
2022-02-03 23:56:46 +0530

    initial commit -v2

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
 
-const bot = require("./src/app");
+const bot = new TelegramBot(process.env.API_TOKEN, { polling: true });
 
-bot.launch();
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, "Received your message");
+});
