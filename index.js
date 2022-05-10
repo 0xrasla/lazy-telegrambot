02e9572ac7e9968867465edd3744a922edad35df
Rasla <programmer.rasla@gmail.com>
2022-05-10 21:52:34 +0530

    New Feautures Added to the bot

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ require("dotenv").config();
 
 const bot = require("./src/app");
 
-bot.launch().then(() => console.log("Bot launched!"));
+const url =
+  process.env.APP_URL || "https://lazybotmakemegobr.herokuapp.com:443";
+bot.launch({
+  webhook: {
+    domain: `${url}/bot${process.env.API_TOKEN}`,
+    port: process.env.PORT,
+  },
+});
 
-// const url =
-//   process.env.APP_URL || "https://lazybotmakemegobr.herokuapp.com:443";
-// bot.launch({
-//   webhook: {
-//     domain: `${url}/bot${process.env.API_TOKEN}`,
-//     port: process.env.PORT,
-//   },
-// });
-
-// // start and stop the bot
-// process.once("SIGINT", () => bot.stop("SIGINT"));
-// process.once("SIGTERM", () => bot.stop("SIGTERM"));
+// start and stop the bot
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
