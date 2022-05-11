697303cdf40742459a79fdf61664f4fc42f7a333
Rasla <programmer.rasla@gmail.com>
2022-05-11 12:15:52 +0530

    Extra lines #23121

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ require("dotenv").config();
 
 const bot = require("./src/app");
 
-bot.launch().then(() => console.log("Bot launched"));
+const url = process.env.APP_URL || "https://lazytelebot001.herokuapp.com:443";
+bot.launch({
+  webhook: {
+    domain: `${url}/bot${process.env.API_TOKEN}`,
+    port: process.env.PORT,
+  },
+});
 
-// const url = process.env.APP_URL || "https://lazytelebot001.herokuapp.com:443";
-// bot.launch({
-//   webhook: {
-//     domain: `${url}/bot${process.env.API_TOKEN}`,
-//     port: process.env.PORT,
-//   },
-// });
-
-// // start and stop the bot
-// process.once("SIGINT", () => bot.stop("SIGINT"));
-// process.once("SIGTERM", () => bot.stop("SIGTERM"));
+// start and stop the bot
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
